7368188f2664cdd4a09a5437f67d05ccf9a264a6
jamsdal <jdaley0502@gmail.com>
2026-08-21 11:43:38 -0600

    Exercise 2.13

--- part2/Phonebook/src/App.jsx
+++ part2/Phonebook/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import personService from './services/persons'
 
 const Filter = ({ value, onChange }) => {
   return (
@@ -47,11 +47,8 @@ const App = () => {
   const [filter, setFilter] = useState('')
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
-      })
+    personService.getAll()
+      .then((persons) => setPersons(persons))
     }, [])
 
   const peopleToShow = persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
@@ -73,10 +70,8 @@ const App = () => {
       number: newNumber
     }
 
-    axios.post("http://localhost:3001/persons", personObject)
-      .then(response =>{
-        setPersons(persons.concat(response.data))
-      })
+    personService.create(personObject)
+      .then(newPerson => setPersons(persons.concat(newPerson)))
   }
 
   const handleNameChange = (event) => {
