d5fe6ada529a9cf13718062a8838872183bd4915
jamsdal <jdaley0502@gmail.com>
2026-08-23 17:14:03 -0600

    Exercise 3.5

--- part3/Phonebook/index.js
+++ part3/Phonebook/index.js
@@ -62,6 +62,20 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    const person = {
+        id: Math.floor(Math.random() * 1000).toString(),
+        name: body.name,
+        number: body.number
+    }
+
+    phonebook = phonebook.concat(person)
+
+    response.json(person)
+})
+
 
 const PORT = 3001
 app.listen(PORT, ()=>console.log(`Server running on port ${PORT}`))
