b5efa5b51052d1b90debfa4358c137ecda7b9496
jamsdal <jdaley0502@gmail.com>
2026-08-24 09:10:54 -0600

    Exercise 3.8

--- part3/Phonebook/index.js
+++ part3/Phonebook/index.js
@@ -26,8 +26,10 @@ let phonebook =
     }
 ]
 
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
