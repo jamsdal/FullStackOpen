c031bf39bab24c9cf6607c7bbaa42d6286f71953
jamsdal <jdaley0502@gmail.com>
2026-08-24 15:42:25 -0600

    render for 3.11

--- part3/Phonebook/index.js
+++ part3/Phonebook/index.js
@@ -30,6 +30,7 @@ morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('dist'))
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
