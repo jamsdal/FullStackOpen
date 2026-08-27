d2b086ad53f9e9b89acf79a118bfbf0e50234e43
jamsdal <jdaley0502@gmail.com>
2026-08-27 13:51:57 -0600

    Learning Part4b

--- part4/LearningPart4/backend/app.js
+++ part4/LearningPart4/backend/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const middleware = require('./utils/middleware')
+const middleware = require('./utils/middlewares')
 const notesRouter = require('./controllers/notes')
 
 const app = express()
